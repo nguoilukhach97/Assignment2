732afcd3ae5ea6871e0e0cc52d86f88598a5486c
hungth230297@gmail.com <hungth230297@gmail.com>
2020-07-08 09:51:36 +0700

    add swagger

--- Assignment2.BackendAPI/Startup.cs
+++ Assignment2.BackendAPI/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Assignment2.BackendAPI
 {
@@ -34,6 +35,11 @@ namespace Assignment2.BackendAPI
             services.AddTransient<IStudentService, StudentService>();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Student solution", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +55,12 @@ namespace Assignment2.BackendAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Assignment V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {

60faa8c30d887317cfe6cbad049229f380d36ba9
hungth230297@gmail.com <hungth230297@gmail.com>
2020-07-08 17:56:47 +0700

    create api student

--- Assignment2/Services/StudentAPI.cs
+++ Assignment2/Services/StudentAPI.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Assignment2.Services
 {
     public class StudentAPI : IStudentAPI
     {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _accessor;
+
+        //public StudentAPI(IHttpClientFactory httpClient, IHttpContextAccessor accessor,IConfiguration configuration)
+        //    : base(httpClient,accessor,configuration)
+        //{
+            
+        //}
+
+        
     }
 }
